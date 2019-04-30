87e140d7957ac6bc2ecd48e2002c2231badb9070
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2019-04-30 16:52:59 -0400

    Taller 2 parte

--- src/components/calc_operation.js
+++ src/components/calc_operation.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { calcOperation } from '../actions/calcActions';
 
-export default class CalcOperation extends Component {
+class CalcOperation extends Component {
   render(){
     return (
-      <div style={{display:'inline-block', minWidth: 20, minHeight:20, backgroundColor:'red'}}>
+      <div onClick={()=>{this.props.doOperation(this.props.calcOp)}} style={{display:'inline-block', minWidth: 20, minHeight:20, backgroundColor:'red'}}>
         <p>{this.props.calcOp}</p>
       </div>
     );
   }
-}
+};
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapActionToProps = dispatch => ({
+  doOperation: calcOperation(dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapActionToProps
+)(CalcOperation);
--- src/reducers/calc.js
+++ src/reducers/calc.js
@@ -1,9 +1,9 @@
-import { ADD_NUMBER } from "../actions/actionTypes";
+import { ADD_NUMBER, DO_OPERATION } from "../actions/actionTypes";
 
 export default (
   state = {
-    previousNumber: 0,
-    currentOperation: '+',
+    previousNumber: null,
+    currentOperation: null,
     display: '0'
   }, action)=>{
     switch(action.type){
@@ -17,6 +17,37 @@ export default (
           return {
             ...state,
             display : state.display + "" + action.payload.digit
+          }
+        }
+      case DO_OPERATION:
+        if(state.previousNumber != null && state.currentOperation != null){
+          let calcResult = 0;
+          switch(state.currentOperation){
+            case '+': calcResult = state.previousNumber + parseInt(state.display);break;
+            case '-': calcResult = state.previousNumber - parseInt(state.display);break;
+            case '*': calcResult = state.previousNumber * parseInt(state.display);break;
+            case '/': calcResult = state.previousNumber / parseInt(state.display);break;
+            default: calcResult = state.previousNumber + parseInt(state.display);break;
+          }
+
+          if(action.payload.operation == '='){
+            return {
+              currentOperation: null,
+              previousNumber: null,
+              display: calcResult
+            }
+          }else{
+            return {
+              previousNumber: calcResult,
+              currentOperation: action.payload.operation,
+              display: '0'
+            }
+          }
+        }else{
+          return {
+            previousNumber: parseInt(state.display),
+            currentOperation: action.payload.operation,
+            display: '0'
           }
         }
       default: return state;
